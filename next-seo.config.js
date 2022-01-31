700da042e0ac00610abb4a9f226a60be76a0ffcc
Diogo <43014908+okaydiogo@users.noreply.github.com>
2022-01-31 21:01:00 +0000

    improve seo

---
+++ next-seo.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Twisty",
+  description: "Explore the web with anyone in real-time.",
+  openGraph: {
+    type: "website",
+    url: "https://twistyapp.org/",
+    site_name: "Twisty",
+    title: "Twisty",
+    description: "Explore the web with anyone in real-time.",
+    images: [
+      {
+        url: "/assets/banner.jpg",
+        width: 1200,
+        height: 624,
+        alt: "",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@okaydiogo",
+    site: "@twistyapp",
+    cardType: "summary_large_image",
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
+import SEO from "../../next-seo.config";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
