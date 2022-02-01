ae4ad86a6308926aae0198651d82ea4e56a3c85a
Diogo <43014908+okaydiogo@users.noreply.github.com>
2022-02-01 20:33:12 +0000

    redirects

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/app",
+        destination: "https://orbit.twistyapp.org",
+        permanent: true,
+      },
+      {
+        source: "/status",
+        destination: "https://status.twistyapp.org",
+        permanent: true,
+      },
+    ];
+  },
 };
