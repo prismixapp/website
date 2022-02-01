53e76ed9835c712c585c6867ea7554c9fee054d2
Diogo <43014908+okaydiogo@users.noreply.github.com>
2022-02-01 20:33:24 +0000

    legal pages

---
+++ pages/privacy.tsx
@@ -0,0 +1,219 @@
+import { Layout } from "../components/Layout";
+
+export default function Privacy() {
+  return (
+    <Layout title="Privacy Policy">
+      <div className="relative py-16 bg-white overflow-hidden">
+        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+          <div
+            className="relative h-full text-lg max-w-prose mx-auto"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute top-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+              />
+            </svg>
+            <svg
+              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute bottom-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <h1>
+              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                Legal
+              </span>
+              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Privacy Policy
+              </span>
+            </h1>
+          </div>
+          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <h2>Summary</h2>
+            <p>
+              While we would love you to read the whole policy, we know that not
+              everyone has time for it. So we have made a summary with some of
+              the most important things.
+            </p>
+            <ul>
+              <li>
+                Twisty won&apos;t sell your personal data to third parties for
+                any reason.
+              </li>
+              <li>
+                You have total control of the data you submit, upload, or post
+                to Twisty.
+              </li>
+              <li>
+                All data exchanged between you and Twisty&apos;s servers is
+                encrypted.
+              </li>
+            </ul>
+            <h2>Our business model</h2>
+            <p>
+              Twisty&apos;s business does not take any interest in your personal
+              data. We make our revenue by user&apos;s donations.
+            </p>
+            <h2>What personal data do we collect?</h2>
+            <p>
+              <span className="font-semibold">Account:</span> We collect and
+              store the information you provide to us, such as your display
+              name, email, username, and photo.
+            </p>
+            <p>
+              <span className="font-semibold">Files and images:</span> When you
+              upload files or images to Twisty, it is sent to a Google Cloud
+              Storage bucket and encrypted using Google-managed encryption keys.
+              When you delete a file or image, we send a deletion request to
+              Google Cloud Storage for all files attached.
+            </p>
+            <h2>Who is this information shared with?</h2>
+            <p>
+              <span className="font-semibold">Service providers:</span> We use
+              the following service providers to process your data:
+            </p>
+            <ul>
+              <li>
+                <a
+                  href="https://cloud.google.com/"
+                  className="text-indigo-500 font-semibold"
+                >
+                  Google Cloud Platform
+                </a>{" "}
+                for content storage
+              </li>
+              <li>
+                <a
+                  href="https://vercel.com/"
+                  className="text-indigo-500 font-semibold"
+                >
+                  Vercel
+                </a>{" "}
+                for website hosting
+              </li>
+            </ul>
+            <p>
+              <span className="font-semibold">Law & Order:</span> We disclose
+              your information to third parties if we determine that such
+              disclosure is reasonably necessary to: (a) comply with a valid
+              legal process; (b) protect any person from death or serious
+              injury; (c) prevent fraud or abuse; or (d) protect our rights,
+              property, safety or interest.
+            </p>
+            <h2>Where do we store your information?</h2>
+            <p>
+              The information above is processed in Portugal only. Exceptions to
+              this are our RTC servers which provide voice & video calls that
+              are hosted in different parts of the world to provide a low
+              latency experience.
+            </p>
+            <h2>Other questions?</h2>
+            <p>
+              If you have any questions, don&apos;t hesitate to email us at{" "}
+              <a
+                className="text-indigo-500 font-semibold"
+                href="mailto:support@twistyapp.org"
+              >
+                support@twistyapp.org
+              </a>{" "}
+              we typically reply within 7 days.
+            </p>
+            <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              Last revised: February 2nd, 2022
+            </span>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
---
+++ pages/terms.tsx
@@ -0,0 +1,206 @@
+import { Layout } from "../components/Layout";
+
+export default function Terms() {
+  return (
+    <Layout title="Terms of Service">
+      <div className="relative py-16 bg-white overflow-hidden">
+        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+          <div
+            className="relative h-full text-lg max-w-prose mx-auto"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute top-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+              />
+            </svg>
+            <svg
+              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute bottom-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <h1>
+              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                Legal
+              </span>
+              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Terms of Service
+              </span>
+            </h1>
+            <p className="mt-8 text-xl text-gray-500 leading-8">
+              Our{" "}
+              <a className="text-indigo-500 font-semibold" href="/privacy">
+                privacy policy
+              </a>{" "}
+              also governs your use of Twisty and explains how we collect and
+              safeguard information that results from your use of Twisty. If you
+              do not agree with (or cannot comply with) these terms and our
+              privacy policy, then you may not use Twisty.
+            </p>
+          </div>
+          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <h2>Content</h2>
+            <p>
+              The content found on Twisty (except of third-party content found
+              when chatting) is property of Twisty or used with permission. You
+              may not distribute, modify, transmit, reuse, download, repost,
+              copy or use it without express written permission from us.
+            </p>
+            <h2>No use by minors</h2>
+            <p>
+              Twisty is intended only for use by individuals at least thirteen
+              (13) years old. By accessing or using Twisty, you warrant and
+              represent that you are at least thirteen (13) years old and with
+              the full authority, right, and capacity to enter into this
+              agreement by all of the terms and conditions. If you are not at
+              least thirteen (13) years old, you are prohibited from both access
+              and usage of Twisty.
+            </p>
+            <h2>Accounts</h2>
+            <p>
+              When you create an account, you guarantee that you are above the
+              age of thirteen (13) and that the information you provide us is
+              accurate, complete, and current at all times. Inaccurate,
+              incomplete, or obsolete information may result in the immediate
+              termination of your account.
+            </p>
+            <p>
+              You are responsible for maintaining the confidentiality of your
+              account and password, including but not limited to the restriction
+              of access to your computer and/or account. You agree to accept
+              responsibility for any and all activities or actions that occur
+              under your account and/or password. You must notify us immediately
+              upon becoming aware of any breach of security or unauthorized use
+              of your account.
+            </p>
+            <p>
+              You may not use as a username the name of another person or entity
+              or that is not lawfully available for use, a name or trademark
+              that is subject to any rights of another person or entity other
+              than you, without appropriate authorization. You may not use as a
+              username any name that is offensive, vulgar, or obscene.
+            </p>
+            <p>
+              We reserve the right to refuse service, terminate accounts, remove
+              or edit content, or cancel orders.
+            </p>
+            <p>
+              If you would like to delete your account, please email us at{" "}
+              <a
+                className="text-indigo-500 font-semibold"
+                href="mailto:support@twistyapp.org"
+              >
+                support@twistyapp.org
+              </a>{" "}
+              with the username belonging to the account you want to be deleted.
+            </p>
+            <h2>Other questions?</h2>
+            <p>
+              If you have any questions, don&apos;t hesitate to email us at{" "}
+              <a
+                className="text-indigo-500 font-semibold"
+                href="mailto:support@twistyapp.org"
+              >
+                support@twistyapp.org
+              </a>{" "}
+              we typically reply within 7 days.
+            </p>
+            <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              Last revised: February 2nd, 2022
+            </span>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
